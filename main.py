4abc51bdbdac17b923ebd0879d020204f3def529
Diganta Kr Banik <digantabanik2000@gmail.com>
2021-06-22 20:59:21 +0530

    ADDED USER NAME INPUT AND WINNER CRITERIA AND DISPLAY

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("pong")
 
+player_1=screen.textinput("PLAYER DETAILS","PLAYER 1 NAME")
+player_2=screen.textinput("PLAYER DETAILS","PLAYER 2 NAME")
+
+final_winner = Turtle()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-scoreboard=Scoreboard()
+scoreboard=Scoreboard(player_1,player_2)
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -24,10 +28,17 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
+def winner_print(winner):
+    final_winner.penup()
+    final_winner.color("white")
+    final_winner.write("The Winner is "+winner,align="center",font=("Courier",30,"normal"))
+
+
 while game_is_on:
     time.sleep(ball.move_speed)
     screen.update()
     ball.ball_move()
+
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
@@ -41,5 +52,10 @@ while game_is_on:
     if ball.xcor()<-380:
         ball.reset()
         scoreboard.update_r_score()
-
+    if(scoreboard.l_score==4):
+        winner_print(player_1)
+        game_is_on=False
+    if(scoreboard.r_score==4):
+        winner_print(player_2)
+        game_is_on=False
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,21 +1,24 @@
 from turtle import Turtle
 
 class Scoreboard(Turtle):
-    def __init__(self):
+    def __init__(self,playe_1,playe_2):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.l_score=0
         self.r_score=0
+        self.player_1=playe_1
+        self.player_2=playe_2
         self.update_scoreboard()
+
 
     def update_scoreboard(self):
         self.clear()
         self.goto(-100,200)
-        self.write(self.l_score,align="center",font=("Courier",80,"normal"))
+        self.write(self.player_1+":"+str(self.l_score),align="center",font=("Courier",80,"normal"))
         self.goto(100,200)
-        self.write(self.r_score,align="center",font=("Courier",80,"normal"))
+        self.write(self.player_2+":"+str(self.r_score),align="center",font=("Courier",80,"normal"))
 
     def update_l_score(self):
         self.l_score+=1
